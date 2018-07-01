9f2543a5a0c3d028f27398be687e5de13f61cc1a
Famae Pascua <2143735@slu.edu.ph>
2018-07-01 13:43:11 -0700

    dbase

--- dbase/geosciences.sql
+++ dbase/geosciences.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 25, 2018 at 02:21 AM
+-- Generation Time: Jul 01, 2018 at 10:41 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
@@ -27,6 +27,20 @@ USE `geosciences`;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `actionslip`
+--
+
+CREATE TABLE `actionslip` (
+  `actionslipID` int(10) NOT NULL,
+  `action` varchar(200) DEFAULT NULL,
+  `actionDesired` varchar(200) DEFAULT NULL,
+  `oicrd` varchar(120) NOT NULL,
+  `note` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `barangay`
 --
 
@@ -35,6 +49,68 @@ CREATE TABLE `barangay` (
   `name` varchar(45) NOT NULL,
   `folderNumber` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barangay`
+--
+
+INSERT INTO `barangay` (`barangayID`, `name`, `folderNumber`) VALUES
+(1, 'Asin Road', 1),
+(2, 'San Luis', 1),
+(3, 'Balacbac', 2),
+(4, 'Sto. Tomas', 2),
+(5, 'Camp 7', 3),
+(6, 'Camp 8', 3),
+(7, 'Bakakeng', 4),
+(8, 'Balsigan', 5),
+(9, 'Poliwes', 5),
+(10, 'San Vicente', 5),
+(11, 'Dominican', 6),
+(12, 'Crystal Cave', 6),
+(13, 'Mirador Hill', 6),
+(14, 'Brookside', 7),
+(15, 'New Lucban', 7),
+(16, 'Trancoville', 7),
+(17, 'Sanitary Camp', 7),
+(18, 'T.Alonzo', 7),
+(19, 'Dontogan', 8),
+(20, 'Fairview', 9),
+(21, 'Irisan', 10),
+(22, 'Pinsao', 11),
+(23, 'Longlong', 12),
+(24, 'Outlook Drive', 13),
+(25, 'Loakan', 14),
+(26, 'Holy Ghost', 15),
+(27, 'Rimando Road', 15),
+(28, 'Aurora Hill', 15),
+(29, 'Pinget', 16),
+(30, 'Dizon Subdivision', 16),
+(31, 'Lourdes Barangay', 17),
+(32, 'Pacdal', 18),
+(33, 'Navy Base', 18),
+(34, 'Gabriela Silang', 19),
+(35, 'Quezon Hill', 20),
+(36, 'Quirino Hill', 21),
+(37, 'P.Burgos', 22),
+(38, 'Cresencia Village', 22),
+(39, 'Camp Allen', 22),
+(40, 'Guisad', 22),
+(41, 'Ferguson Road', 22),
+(42, 'Kitma', 23),
+(43, 'City Camp', 23),
+(44, 'Queen of Peace', 23),
+(45, 'Campo Sioco', 23),
+(46, 'Palma', 23),
+(47, 'South Drive', 24),
+(48, 'Cabinet Hill', 24),
+(49, 'Salud Mitra', 24),
+(50, 'Lualhati', 24),
+(51, 'General Luna', 24),
+(52, 'Bontoc', 25),
+(53, 'La Trinidad', 26),
+(54, 'Other Barangays', 27),
+(55, 'Tuba', 28),
+(56, 'Other Municipalities', 29);
 
 -- --------------------------------------------------------
 
@@ -66,6 +142,19 @@ CREATE TABLE `logs` (
   `releaseID` int(10) DEFAULT NULL,
   `recordID` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `logs`
+--
+
+INSERT INTO `logs` (`logID`, `logDate`, `logTime`, `activity`, `userID`, `receiveID`, `unclaimID`, `releaseID`, `recordID`) VALUES
+(1, '0000-00-00', '00:00:00', '', 1, NULL, NULL, NULL, NULL),
+(2, '0000-00-00', '00:00:00', '', 1, NULL, NULL, NULL, NULL),
+(3, '0000-00-00', '00:00:00', '', 1, NULL, NULL, NULL, NULL),
+(4, '0000-00-00', '00:00:00', '', 1, NULL, NULL, NULL, NULL),
+(5, '0000-00-00', '00:00:00', '', 1, NULL, NULL, NULL, NULL),
+(6, '0000-00-00', '00:00:00', '', 1, NULL, NULL, NULL, NULL),
+(7, '0000-00-00', '00:00:00', '', 1, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -144,8 +233,22 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userID`, `username`, `password`, `firstName`, `lastName`, `userType`) VALUES
+(1, 'adminmark', 'admin01', 'Raymark', 'Cuenca', 'admin'),
+(2, 'famaepascua', 'user1', 'Fatima Mae', 'Pascua', 'user');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `actionslip`
+--
+ALTER TABLE `actionslip`
+  ADD PRIMARY KEY (`actionslipID`);
 
 --
 -- Indexes for table `barangay`
@@ -157,37 +260,51 @@ ALTER TABLE `barangay`
 -- Indexes for table `location`
 --
 ALTER TABLE `location`
-  ADD PRIMARY KEY (`locationID`);
+  ADD PRIMARY KEY (`locationID`),
+  ADD KEY `barangay` (`barangayID`);
 
 --
 -- Indexes for table `logs`
 --
 ALTER TABLE `logs`
-  ADD PRIMARY KEY (`logID`);
+  ADD PRIMARY KEY (`logID`),
+  ADD KEY `user` (`userID`),
+  ADD KEY `received_docu` (`receiveID`),
+  ADD KEY `release_docu` (`releaseID`),
+  ADD KEY `unclaim_docu` (`unclaimID`),
+  ADD KEY `record` (`recordID`);
 
 --
 -- Indexes for table `receive`
 --
 ALTER TABLE `receive`
-  ADD PRIMARY KEY (`receiveID`);
+  ADD PRIMARY KEY (`receiveID`),
+  ADD KEY `location` (`locationID`),
+  ADD KEY `action_slip` (`actionslipID`);
 
 --
 -- Indexes for table `records`
 --
 ALTER TABLE `records`
-  ADD PRIMARY KEY (`recordID`);
+  ADD PRIMARY KEY (`recordID`),
+  ADD KEY `received` (`receiveID`),
+  ADD KEY `unclaimed` (`unclaimID`),
+  ADD KEY `release` (`releaseID`);
 
 --
 -- Indexes for table `release`
 --
 ALTER TABLE `release`
-  ADD PRIMARY KEY (`releaseID`);
+  ADD PRIMARY KEY (`releaseID`),
+  ADD KEY `unclaimed_doc` (`unclaimID`),
+  ADD KEY `received_doc` (`receiveID`);
 
 --
 -- Indexes for table `unclaim`
 --
 ALTER TABLE `unclaim`
-  ADD PRIMARY KEY (`unclaimID`);
+  ADD PRIMARY KEY (`unclaimID`),
+  ADD KEY `received_docs` (`receiveID`);
 
 --
 -- Indexes for table `users`
@@ -200,10 +317,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `actionslip`
+--
+ALTER TABLE `actionslip`
+  MODIFY `actionslipID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `barangay`
 --
 ALTER TABLE `barangay`
-  MODIFY `barangayID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `barangayID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `location`
@@ -215,7 +338,7 @@ ALTER TABLE `location`
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `logID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `logID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `receive`
@@ -245,7 +368,55 @@ ALTER TABLE `unclaim`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `userID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `location`
+--
+ALTER TABLE `location`
+  ADD CONSTRAINT `barangay` FOREIGN KEY (`barangayID`) REFERENCES `barangay` (`barangayID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `logs`
+--
+ALTER TABLE `logs`
+  ADD CONSTRAINT `received_docu` FOREIGN KEY (`receiveID`) REFERENCES `receive` (`receiveID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `record` FOREIGN KEY (`recordID`) REFERENCES `records` (`recordID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `release_docu` FOREIGN KEY (`releaseID`) REFERENCES `release` (`releaseID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `unclaim_docu` FOREIGN KEY (`unclaimID`) REFERENCES `unclaim` (`unclaimID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `user` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `receive`
+--
+ALTER TABLE `receive`
+  ADD CONSTRAINT `action_slip` FOREIGN KEY (`actionslipID`) REFERENCES `actionslip` (`actionslipID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `location` FOREIGN KEY (`locationID`) REFERENCES `location` (`locationID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `records`
+--
+ALTER TABLE `records`
+  ADD CONSTRAINT `received` FOREIGN KEY (`receiveID`) REFERENCES `receive` (`receiveID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `release` FOREIGN KEY (`releaseID`) REFERENCES `release` (`releaseID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `unclaimed` FOREIGN KEY (`unclaimID`) REFERENCES `unclaim` (`unclaimID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `release`
+--
+ALTER TABLE `release`
+  ADD CONSTRAINT `received_doc` FOREIGN KEY (`receiveID`) REFERENCES `receive` (`receiveID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `unclaimed_doc` FOREIGN KEY (`unclaimID`) REFERENCES `unclaim` (`unclaimID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `unclaim`
+--
+ALTER TABLE `unclaim`
+  ADD CONSTRAINT `received_docs` FOREIGN KEY (`receiveID`) REFERENCES `receive` (`receiveID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
