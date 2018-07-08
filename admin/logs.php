b118c461471b3b9e914f4af63b9b75bf5023f600
yanzky <yanzkytootsky@gmail.com>
2018-07-08 15:16:46 +0800

    Hakuna Matata

--- admin/logs.php
+++ admin/logs.php
@@ -1,25 +1,20 @@
 <?php
 session_start();
-
-if(!isset($_SESSION['currentUser'])){
+if (!isset($_SESSION['currentUser'])) {
     $m = "Please Login First";
-    echo "<script type='text/javascript'>";
-    echo "alert($m)";
-    echo "window.location.replace('../index.php');";
-    echo "</script>";
-
+    echo "<script type='text/javascript'>
+    alert('$m');
+    window.location.replace('../index.php');
+    </script>";
 }
-
-if($_SESSION['currentUserType'] != "user"){
-    $m = "Forbidden Access";
-    echo "<script type='text/javascript'>";
-    echo "alert($m)";
-    echo "window.location.replace('../index.php');";
-    echo "</script>";
-
+if ($_SESSION['currentUserType'] == "user") {
+    session_destroy();
+    $m = "Unauthorized Access";
+    echo "<script type='text/javascript'>
+    alert('$m');
+    window.location.replace('../index.php');
+    </script>";
 }
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
